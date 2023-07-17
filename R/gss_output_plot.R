66aa58311acf977d2ca255299e83682649f4982d
moi-taiga <moi.t.nicholas@gmail.com>
2023-07-17 13:37:16 +0100

    added the option to include the switching point of chosen gene / genes.

--- R/gss_output_plot.R
+++ R/gss_output_plot.R
@@ -10,19 +10,20 @@
 #' @return nice plot highlighting the probable position of your sample on your trajectory.
 #' @export
 #'
-gss_output_plot <- function(sample.gss, col = "red", overlay = FALSE){
+gss_output_plot <- function(sample.gss, col = "red", overlay = FALSE, label = "sample name", genes_of_interest = c("ENSG00000248605","ENSG00000272168")){
 
  if (!overlay) {
   plot(x = 1:100,
        y = (sample.gss$sample_flat/max(sample.gss$sample_flat)*100),
-       ylim = c(0,100),
+       ylim = c(0,110),
+       xlim = c(0,100),
        pch = 20,
        cex = 0.8,
        col = col,
        type = "p",
        xlab = "Pseudo-Time Index",
        ylab = "GSS Score",
-       main = "Predicted Position of Sample")
+       main = paste("Predicted Positions"))
 
   segments(which.max(colSums(sample.gss$sample_flat)),
            -3.9,
@@ -31,24 +32,83 @@ gss_output_plot <- function(sample.gss, col = "red", overlay = FALSE){
            lwd = 1,
            lty = 2,
            col = col)
- } else {
-   lines( x = 1:100,
-          y = (sample.gss$sample_flat/max(sample.gss$sample_flat)*100),
-        pch = 20,
-        cex = 0.8,
-        col = col,
-        type = "p")
 
-   segments(which.max(colSums(sample.gss$sample_flat)),
-            -3.9,
-            which.max(colSums(sample.gss$sample_flat)),
-            (sample.gss$sample_flat[which.max(colSums(sample.gss$sample_flat))]/max(sample.gss$sample_flat)*100),
-            lwd=1,
-            lty = 2,
-            col = col)
+  text(x = which.max(colSums(sample.gss$sample_flat)),
+       y = (sample.gss$sample_flat[which.max(colSums(sample.gss$sample_flat))]/max(sample.gss$sample_flat)*100),
+       labels = label,
+       col = col,
+       pos = 3)
+
+  if (length(genes_of_interest) > 0) {
+    for (gene_name in genes_of_interest) {
+      segments(switching_genes[gene_name,"switch_at_timeidx"],
+               -3.9,
+               switching_genes[gene_name,"switch_at_timeidx"],
+               -0.5,
+               lwd = 1,
+               lty = 2)
+
+      text(x = switching_genes[gene_name,"switch_at_timeidx"] + 3,
+           y = 1,
+           labels = gene_name,
+           srt = -20,
+           cex = 0.86,
+           pos = 2)
+
+    }
+  }
+
+
+
+  } else {
+    lines(x = 1:100,
+         y = (sample.gss$sample_flat/max(sample.gss$sample_flat)*100),
+         ylim = c(0,110),
+         xlim = c(0,100),
+         pch = 20,
+         cex = 0.8,
+         col = col,
+         type = "p")
+
+    segments(which.max(colSums(sample.gss$sample_flat)),
+             -3.9,
+             which.max(colSums(sample.gss$sample_flat)),
+             (sample.gss$sample_flat[which.max(colSums(sample.gss$sample_flat))]/max(sample.gss$sample_flat)*100),
+             lwd = 1,
+             lty = 2,
+             col = col)
+
+    text(x = which.max(colSums(sample.gss$sample_flat)),
+         y = (sample.gss$sample_flat[which.max(colSums(sample.gss$sample_flat))]/max(sample.gss$sample_flat)*100),
+         labels = label,
+         col = col,
+         pos = 3)
+
+    if (length(genes_of_interest) > 0) {
+      for (gene_name in genes_of_interest) {
+        segments(switching_genes[gene_name,"switch_at_timeidx"],
+                 -3.9,
+                 switching_genes[gene_name,"switch_at_timeidx"],
+                 -0.5,
+                 lwd = 1,
+                 lty = 2)
+
+        text(x = switching_genes[gene_name,"switch_at_timeidx"] + 3,
+             y = 1,
+             labels = gene_name,
+             srt = -20,
+             cex = 0.86,
+             pos = 2)
+
    }
-
+  }
+ }
 }
+
+
+
+
+
 
 
 
