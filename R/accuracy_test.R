31627413257e74914b9a4be3ce7351c6cca74646
moi-taiga <moi.t.nicholas@gmail.com>
2023-07-17 14:31:57 +0100

    renamed accuracy column to innacuracy. It's a more accurate name.

--- R/accuracy_test.R
+++ R/accuracy_test.R
@@ -16,7 +16,7 @@ score_gss_accuracy <- function(gss_obj, reference.sce) {
     true_position_of_cells_pseudotime = reference.sce@colData$slingPseudotime_1,  # True pseudotime values from reference.sce
     true_position_of_cells_timeIDX = NA,                                          # Placeholder for true time indices
     predicted_position_of_cells_timeIDX = NA,                                     # Placeholder for predicted time indices
-    accuracy = NA                                                                 # Placeholder for accuracy values
+    inaccuracy = NA                                                               # Placeholder for inaccuracy values
   )
 
   # Calculate the time step based on the pseudotime range
@@ -30,7 +30,7 @@ score_gss_accuracy <- function(gss_obj, reference.sce) {
   accuracy$predicted_position_of_cells_timeIDX[match(names(apply(gss_obj$cells_flat, 1, which.max)), accuracy$cell_names)] <- apply(gss_obj$cells_flat, 1, which.max)
 
   # Calculate the accuracy as the absolute difference between the true and predicted time indices
-  accuracy$accuracy <- abs(accuracy$true_position_of_cells_timeIDX - accuracy$predicted_position_of_cells_timeIDX)
+  accuracy$inaccuracy <- abs(accuracy$true_position_of_cells_timeIDX - accuracy$predicted_position_of_cells_timeIDX)
 
   return(accuracy)
 }
