e656128b3842cedde86c2faff4827ab3f649fbcd
moi-taiga <moi.t.nicholas@gmail.com>
2024-03-19 13:51:34 +0000

    change default behaviour for printing PPR_OBJECTS

--- R/ppr_utils.R
+++ R/ppr_utils.R
@@ -1,13 +1,24 @@
-#' @title Print PPR Object Summary
+# R/utils.R
+
+#' PPR_OBJECT Class Definition
+#'
+#' @export
+setClass("PPR_OBJECT")
+
+#' @title Print method for PPR Object.
 #'
 #' @description
 #' Make calling the PPR_OBJECT nicer by printing a summary of its contents.
 #'
 #' @param x An object of class 'PPR_OBJECT' to be summarized.
 #'
-#' @return A summary of the contents of the 'PPR_OBJECT'.
+#' @return A summary of the contents of the object.
 #' @export
 print.PPR_OBJECT <- function(x) {
+  if (!inherits(x, "PPR_OBJECT")) {
+    stop("Input is not of class 'PPR_OBJECT'")
+  }
+
   cat("This is an object of class 'PPR_OBJECT'\n")
   cat("It contains", length(x), "elements:\n\n")
 
@@ -18,18 +29,12 @@ print.PPR_OBJECT <- function(x) {
     } else if (is.matrix(x[[e]])) {
       cat("Element", e, ":", names(x[e]), "\n")
       cat("A Matrix with dimensions of", paste(dim(x[[e]]), collapse = " x "), "\n\n")
+    } else {
+      cat("Element", e, ":", names(x[e]), "\n")
+      cat("Type:", class(x[[e]]), "\n\n")
     }
   }
 }
 
-# print.PPR_OBJECT_default <- function(x, ...) {
-#   NextMethod()
-# }
-#
-# print <- function(x, ...) {
-#   if (inherits(x, "PPR_OBJECT")) {
-#     print.PPR_OBJECT(x)
-#   } else {
-#     print.PPR_OBJECT_default(x, ...)
-#   }
-# }
+#' Set Default Print Method for PPR_OBJECT
+setMethod("print", "PPR_OBJECT", print.PPR_OBJECT)
