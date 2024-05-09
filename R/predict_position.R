4aee58c58d5101ae58f8fd131eb9234f1068549f
moi-taiga <moi.t.nicholas@gmail.com>
2024-05-09 10:54:19 +0100

    housekeeping

--- R/predict_position.R
+++ R/predict_position.R
@@ -1,16 +1,21 @@
 #' @title PPPR Predict Position
 #'
 #' @description
-#' Produces an estimate for the position on trajectory of each gene in each cell of a sample.
-#'     This can be later aggregated to estimate the position of the sample along the trajectory.
+#' Produces an estimate for the position on trajectory of each cell of a sample.
+#' This is later aggregated to estimate samples position along the trajectory.
 #'
 #' @param sample_sce A Single Cell Experiment object,
 #' containing a matrix of your samples binary gene expression,
 #' which has been filtered to only include switching genes.
-#' @param switching_genes Genes which switch through the trajectory as identified by GeneSwitches.
+#' @param switching_genes Genes which switch through the trajectory,
+#' as identified by GeneSwitches.
 #'
-#' @return A list of matrices: A matrix for each cell where the columns represent progress through a trajectory,
-#'     and the rows represent genes, values indicate a likely position of the cell upon the trajectory based that genes bianrized expression.
+#' @return A list of matrices:
+#' A matrix for each cell:
+#' the columns represent progress through a trajectory,
+#' the rows represent genes,
+#' values indicate a likely position of the cell upon the trajectory,
+#' based that genes bianrized expression.
 #' @export
 #'
 ppr_predict_position <- function(sample_sce, switching_genes) {
@@ -22,7 +27,7 @@ ppr_predict_position <- function(sample_sce, switching_genes) {
          deparse(substitute(sample_sce)),
          "\" \n  You must run `GeneSwitches::binarize_exp()` first.")
   } else {
-  reduced_binary_counts_matrix <- sample_sce@assays@data@listData$binary
+    reduced_binary_counts_matrix <- sample_sce@assays@data@listData$binary
   }
   ## The final output will be a ppr_obj (list) comprised of 3 objects.
   # Make the list of length 3, and name the objects
@@ -32,13 +37,15 @@ ppr_predict_position <- function(sample_sce, switching_genes) {
   class(ppr_obj) <- "PPR_OBJECT"
 
   ## Reorder switching_genes
-  # as the code relies on the rownames and idicies of the genes in reduced_binary_counts_matrix and switching_genes matching.
+  # because the code relies on the rownames and idicies of the genes,
+  # in reduced_binary_counts_matrix and switching_genes matching.
   switching_genes <- switching_genes[rownames(reduced_binary_counts_matrix),]
 
   ## Input values:
   number_of_cells <- dim(reduced_binary_counts_matrix)[2]
-  # Should we get the number of switching genes from here? or dim(reduced_binary_counts_matrix)[1]
-  # It is possible for dim(reduced_binary_counts_matrix)[1] <  dim(switching_genes)[1]
+  # Should we get the number of switching genes from here?
+  # or dim(reduced_binary_counts_matrix)[1]
+  # dim(reduced_binary_counts_matrix)[1] < dim(switching_genes)[1] is possible.
   number_of_switching_genes <- dim(switching_genes)[1]
 
 
@@ -48,29 +55,31 @@ ppr_predict_position <- function(sample_sce, switching_genes) {
   switching_time <- as.numeric(switching_genes$switch_at_timeidx)
   switching_direction <- switching_genes$direction
 
-  # Building the genomic_expression_traces list. (faster than building it dynamically.)
+  # Building an empty genomic_expression_traces list.
+  # (faster than building it dynamically.)
   all_patients_cells_scored <- vector("list", number_of_cells)
   names(all_patients_cells_scored) <- colnames(reduced_binary_counts_matrix)
 
-  # Loop through all cells making matrices for each,
-  #which represent likely position of a cell on a trajectory based on the expression of each gene.
+  # Loop through all cells,
+  # making matrices for each, which represent likely position of a cell (c),
+  # on a trajectory based on the expression of each gene.
   for (c in 1:number_of_cells) {
-    # Build the matrix of 0's which has genes as rows and pseudotime indecies as columns.
-   genomic_expression_mat <- matrix(0, nrow = number_of_switching_genes, ncol = 100)
-   rownames(genomic_expression_mat) <- rownames(reduced_binary_counts_matrix)
-    binarized_gene_expression_for_cell_c <- reduced_binary_counts_matrix[, c]
+    # Build the matrix of 0's
+    # which has genes as rows and pseudotime indecies as columns.
+    genomic_expression_mat <- matrix(0,
+                                    nrow = number_of_switching_genes, 
+                                    ncol = 100)
 
-    up_indices <- which(
-      binarized_gene_expression_for_cell_c == 1 & switching_direction == "up")
+    rownames(genomic_expression_mat) <- rownames(reduced_binary_counts_matrix)
+    bin_exp_c <- reduced_binary_counts_matrix[, c]
 
-    down_indices <- which(
-      binarized_gene_expression_for_cell_c == 0 & switching_direction == "down")
+    up_indices <- which(bin_exp_c == 1 & switching_direction == "up")
 
-    not_up_indices <- which(
-      binarized_gene_expression_for_cell_c == 0 & switching_direction == "up")
+    down_indices <- which(bin_exp_c == 0 & switching_direction == "down")
 
-    not_down_indices <- which(
-      binarized_gene_expression_for_cell_c == 1 & switching_direction == "down")
+    not_up_indices <- which(bin_exp_c == 0 & switching_direction == "up")
+
+    not_down_indices <- which(bin_exp_c == 1 & switching_direction == "down")
 
     for (i in up_indices) {
       genomic_expression_mat[i, switching_time[i]:100] <- 1
@@ -93,14 +102,16 @@ ppr_predict_position <- function(sample_sce, switching_genes) {
 
   ppr_obj$genomic_expression_traces <- all_patients_cells_scored
 
-### GENOMIC EXPRESSION TRACES CREATED
-# Now flatten:
+  ### GENOMIC EXPRESSION TRACES CREATED
+  # Now flatten:
 
-# Use lapply to calculate column sums for each matrix
-ppr_obj$cells_flat <- do.call(rbind, lapply(all_patients_cells_scored, colSums))
-rownames(ppr_obj$cells_flat) <- names(all_patients_cells_scored)
-# Combine each cells column sums into a single flat matrix.
-ppr_obj$sample_flat <- matrix(colSums(ppr_obj$cells_flat), nrow = 1)
+  # Use lapply to calculate column sums for each matrix
+  ppr_obj$cells_flat <- do.call(rbind, 
+                                    lapply(all_patients_cells_scored,
+                                           colSums))
+  rownames(ppr_obj$cells_flat) <- names(all_patients_cells_scored)
+  # Combine each cells column sums into a single flat matrix.
+  ppr_obj$sample_flat <- matrix(colSums(ppr_obj$cells_flat), nrow = 1)
 
-return(ppr_obj)
+  return(ppr_obj)
 }
