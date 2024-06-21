508b03056a8594f865536fd8cd1438e6d2d49c34
moi-taiga <moi.t.nicholas@gmail.com>
2024-06-21 13:01:23 +0100

    improved zscore efficiencly, sce is no longer coppied

--- R/zscore.R
+++ R/zscore.R
@@ -1,60 +1,75 @@
 #' @title zscore
 #'
 #' @description
-#' Calculate the zscore of the raw ppr score.
+#' Calculate the zscore of the raw ppr score,
+#'  by comparing to 2000 random samples.
 #'
 #' @param sce A Single Cell Experiment object,
-#' containing a matrix of your samples binary gene expression,
-#' which has been filtered to only include switching genes,
-#' using PathPinPointR::subset_switching_genes().
+#'  containing a matrix of your samples binary gene expression,
+#'  which has been filtered to only include switching genes,
+#'  using PathPinPointR::subset_switching_genes().
 #' @param ppr An object of class ppr, must be the same sample.
 #' @param switching_genes Genes which switch through the trajectory,
-#' @param cpu The number of cores to use for parallel processing.
+#' @param cpu Number of cores to use for parallel processing,
+#'  (default is one less than available cores).
 #'
-#' @return the zscore of the raw ppr score.
+#' @return the zscore of the samples raw ppr score.
 #'
-#' @importFrom stats shapiro.test
 #' @importFrom parallel makeCluster parLapply stopCluster
 #' @export
 #'
 
 calculate_zscore <- function(sce, ppr, switching_genes, cpu = 0) {
+
+  ## check
+
+  # check that sce is a SingleCellExperiment object
+
+  # check that sce is reduced?
+
+  # check that ppr is a ppr object
+
+  # check that sce and ppr come from the same sample
+
+  # check that switching_genes is a df?
+
+  # if cpu is set to 0 or is larger or equal to than the number available.
+  # use all but one available cores
+  if (cpu == 0 || cpu >= detectCores()) {
+    cpu <- paralell::detectCores() - 1
+  }
+
+
   # Find the maximum raw ppr score.
   max_raw_ppr_score <- max(ppr$sample_flat)
 
-  ## produce 2000 random samples, and extract their max ppr scores.
   # Extract the binary matrix
   bin_mat <- sce@assays@data$binary
 
-  # if cpu is set to 0 use all but one available cores
-  if (cpu == 0) {
-    cpu <- detectCores() - 1
-  }
+  
 
   # Define the number of iterations
   n_iterations <- 2000
 
   # Create a cluster for parallel processing
-  cl <- makeCluster(cpu)
+  cl <- parallel::makeCluster(cpu)
 
   # Cluster setup
-  clusterExport(cl,
-                c("predict_position",
-                  "switching_genes",
-                  "sce",
-                  "which_mid_max"))
+  parallel::clusterExport(cl,
+                          c("predict_position",
+                            "switching_genes",
+                            "which_mid_max",
+                            "sce"))
 
   # Parallel loop using parLapply
   random_max_raw_ppr_scores <- parLapply(cl, 1:n_iterations, function(i) {
 
-    # Copy the SingleCellExperiment object for each iteration
-    sce_copy <- sce
     # Shuffle the row names in place
-    bin_mat <- sce_copy@assays@data$binary
+    bin_mat <- sce@assays@data$binary
     rownames(bin_mat) <- sample(rownames(bin_mat))
-    sce_copy@assays@data$binary <- bin_mat
+    sce@assays@data$binary <- bin_mat
     # Perform prediction
-    random_ppr <- predict_position(sce_copy, switching_genes)
+    random_ppr <- predict_position(sce, switching_genes)
     # Extract the maximum raw ppr score
     max(random_ppr$sample_flat)
   })
