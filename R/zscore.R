0cd1889a387688f0800169595da1f5365b8700f1
moi-taiga <moi.t.nicholas@gmail.com>
2024-06-21 14:48:07 +0100

    Added checks

--- R/zscore.R
+++ R/zscore.R
@@ -7,7 +7,7 @@
 #' @param sce A Single Cell Experiment object,
 #'  containing a matrix of your samples binary gene expression,
 #'  which has been filtered to only include switching genes,
-#'  using PathPinPointR::subset_switching_genes().
+#'  using PathPinpointR::subset_switching_genes().
 #' @param ppr An object of class ppr, must be the same sample.
 #' @param switching_genes Genes which switch through the trajectory,
 #' @param cpu Number of cores to use for parallel processing,
@@ -24,14 +24,41 @@ calculate_zscore <- function(sce, ppr, switching_genes, cpu = 0) {
   ## check
 
   # check that sce is a SingleCellExperiment object
+  if (!is(sce, "SingleCellExperiment")) {
+    stop("\n  The sce argument must be a SingleCellExperiment object.")
+  }
 
-  # check that sce is reduced?
+  # Check if sce contains binarized expression data
+  if (is.null(sce@assays@data@listData$binary)) {
+    # If binarized expression data is missing, display a message and stop.
+    stop("\n  Binarized expression data not found in \"",
+         deparse(substitute(sce)),
+         "\" \n  You must run `GeneSwitches::binarize_exp()` first.")
+  }
+
+  # check that sce has been subset
+  if (dim(sce)[1] > dim(switching_genes)[1]) {
+    stop("\n  The number of genes in the reduced binary counts matrix is
+     greater than the number of switching genes.
+     
+    Make sure you have run PathPinpointR::subset_switching_genes()")
+  }
 
   # check that ppr is a ppr object
+  if (!is(ppr, "PPR_OBJECT")) {
+    stop("\n  The ppr argument must be a PPR_OBJECT.")
+  }
 
   # check that sce and ppr come from the same sample
+  if (dim(sce)[1] != dim(ppr[[1]][[1]])[1] || dim(sce)[2] != length(ppr[[1]])) {
+    stop("\n  The sce and ppr objects must be from the same sample.")
+  }
 
-  # check that switching_genes is a df?
+  # check that switching_genes is a > class(switching_genes)
+  if (is(switching_genes, "DFrame")) {
+    cat("Warning:
+      Make sure switching_genes argument is a DFrame.\n")
+  }
 
   # if cpu is set to 0 or is larger or equal to than the number available.
   # use all but one available cores
@@ -39,14 +66,11 @@ calculate_zscore <- function(sce, ppr, switching_genes, cpu = 0) {
     cpu <- paralell::detectCores() - 1
   }
 
-
   # Find the maximum raw ppr score.
   max_raw_ppr_score <- max(ppr$sample_flat)
 
   # Extract the binary matrix
   bin_mat <- sce@assays@data$binary
-
-  
 
   # Define the number of iterations
   n_iterations <- 2000
@@ -89,5 +113,9 @@ calculate_zscore <- function(sce, ppr, switching_genes, cpu = 0) {
   # calculate the z-score
   z_score <- (max_raw_ppr_score - mean(random_max_raw_ppr_scores)) / sd_random_max_raw_ppr_scores
 
-  return(z_score)
+  # assign Z-score to the ppr object
+  ppr$z_score <- z_score
+  
+  #return the ppr object
+  return(ppr)
 }
