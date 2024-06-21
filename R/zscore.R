c857feb9a3af385378eb1c40523b017565174ba2
moi-taiga <moi.t.nicholas@gmail.com>
2024-06-21 12:21:08 +0100

    parallel + removed norm test as it was too sensitive, norm is an assumption

--- R/zscore.R
+++ R/zscore.R
@@ -9,13 +9,16 @@
 #' using PathPinPointR::subset_switching_genes().
 #' @param ppr An object of class ppr, must be the same sample.
 #' @param switching_genes Genes which switch through the trajectory,
+#' @param cpu The number of cores to use for parallel processing.
 #'
 #' @return the zscore of the raw ppr score.
 #'
+#' @importFrom stats shapiro.test
+#' @importFrom parallel makeCluster parLapply stopCluster
 #' @export
 #'
 
-calculate_zscore <- function(sce, ppr, switching_genes) {
+calculate_zscore <- function(sce, ppr, switching_genes, cpu = 0) {
   # Find the maximum raw ppr score.
   max_raw_ppr_score <- max(ppr$sample_flat)
 
@@ -23,34 +26,47 @@ calculate_zscore <- function(sce, ppr, switching_genes) {
   # Extract the binary matrix
   bin_mat <- sce@assays@data$binary
 
-  # Create a vector to store the max ppr scores of the random samples
-  random_max_raw_ppr_scores <- numeric(2000)
-
-  # predict the position of 2000 random samples
-  # and extract the max ppr score
-  for (i in 1:2000) {
-    # Shuffle the row names in place
-    rownames(bin_mat) <- sample(rownames(bin_mat))
-
-    # Update the binary matrix within the sample object
-    sce@assays@data$binary <- bin_mat
-
-    # Perform prediction
-    random_ppr <- predict_position(sce, switching_genes)
-
-    # Extract the maximum raw ppr score
-    random_max_raw_ppr_scores[i] <- max(random_ppr$sample_flat)
+  # is cpu is set to 0 use all but one available cores
+  if (cpu == 0) {
+    cpu <- detectCores() - 1
   }
+
+  # Define the number of iterations
+  n_iterations <- 2000
+
+  # Create a cluster for parallel processing
+  cl <- makeCluster(cpu)
+
+  # Cluster setup
+  clusterExport(cl,
+                c("predict_position",
+                  "switching_genes",
+                  "sce",
+                  "which_mid_max"))
+
+  # Parallel loop using parLapply
+  random_max_raw_ppr_scores <- parLapply(cl, 1:n_iterations, function(i) {
+
+    # Copy the SingleCellExperiment object for each iteration
+    sce_copy <- sce
+    # Shuffle the row names in place
+    bin_mat <- sce_copy@assays@data$binary
+    rownames(bin_mat) <- sample(rownames(bin_mat))
+    sce_copy@assays@data$binary <- bin_mat
+    # Perform prediction
+    random_ppr <- predict_position(sce_copy, switching_genes)
+    # Extract the maximum raw ppr score
+    max(random_ppr$sample_flat)
+  })
+
+  # Stop the cluster
+  stopCluster(cl)
+
+  # unlist the random_max_raw_ppr_scores (make it a vector)
+  random_max_raw_ppr_scores <- unlist(random_max_raw_ppr_scores)
 
   # calculate the distance of the max_raw_ppr_score from the mean of random_max_raw_ppr_scores
   # using standard deviations of the random samples
-
-  # check that random_max_raw_ppr_scores are normally distributed
-  # Shapiro-Wilk Test
-  norm <- shapiro.test(random_max_raw_ppr_scores)
-  if (norm$p.value < 0.05) {
-    warning("The random_max_raw_ppr_scores are not normally distributed.")
-  }
 
   # calculate the standand deviation of the random max ppr scores
   sd_random_max_raw_ppr_scores <- sd(random_max_raw_ppr_scores)
